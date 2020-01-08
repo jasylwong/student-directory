632da1ff9fdb9c44954d9763894ea42a904fb4de
jasylwong <jasylwong@gmail.com>
2020-01-08 22:35:13 +0000

    Option to load student names from csv file added to interactive menu

---
+++ directory_12.rb
@@ -0,0 +1,90 @@
+@students = []
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save students"
+    puts "4. Load the list from students.csv"
+    puts "9. Exit"
+end
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # get the name first
+    name = gets.chomp
+    # while the name is not empty, repeat this code
+    while !name.empty? do
+        # add the student hash to the array
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
+        # get another name from the user
+        name = gets.chomp
+    end
+end
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "--------------"
+end
+
+def print_students_list
+    @students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+end
+
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_input = [student[:name], student[:cohort]]
+        csv_input = student_input.join(",")
+        file.puts(csv_input)
+    end
+    file.close
+end
+
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, cohort = line.chomp.split(",")
+        @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "3"
+        save_students
+    when "4"
+        load_students
+    when "9"
+        exit
+    else
+        puts "I don't know what you meant, try again."
+    end
+end
+
+def interactive_menu
+    loop do
+        print_menu
+        process(gets.chomp)
+    end
+end
+
+interactive_menu
