2287ee4268f67c58778d017fa351cce5d7557667
jasylwong <jasylwong@gmail.com>
2020-01-13 22:36:33 +0000

    Chapter 8 exercise 7 complete

--- directory_8.7.rb
+++ directory_8.7.rb
@@ -1,17 +1,29 @@
 def input_students
-    puts "Please enter the names of the students"
+    puts "Please enter the names and cohorts of the students"
     puts "To finish, just hit return twice"
     # create an empty array
     students = []
     # get the name first
+    puts "Name: "
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
+        months = %w(January February March April May June July August September October November December) + [""]
+        puts "Cohort: "
+        cohort = gets.chomp
+        while !months.include?(cohort)
+            puts "Please enter a valid month"
+            puts "Cohort: "
+            cohort = gets.chomp
+        end
+        cohort = cohort.empty? ? :november : cohort.downcase
         # add the student hash to the array
-        students << {name: name, cohort: :november}
+        students << {name: name, cohort: cohort.to_sym}
         puts "Now we have #{students.count} students"
         # get another name from the user
+        puts "Name: "
         name = gets.chomp
+
     end
     # return the array of students
     students
