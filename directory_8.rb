a1d5424160453b9324e3f49ab98ff00858b9320f
jasylwong <jasylwong@gmail.com>
2020-01-16 11:19:25 +0000

    Further incorporation of all chapter 8 exercises into one file.

--- directory_8.rb
+++ directory_8.rb
@@ -1,11 +1,23 @@
+@months = %w(January February March April May June July August September October November December)
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    months = %w(January February March April May June July August September October November December) + [""]
+    puts "Cohort: "
+    cohort = gets.chomp
+    while !months.include?(cohort)
+      puts "Please enter a valid month"
+      puts "Cohort: "
+      cohort = gets.chomp
+    end
+    cohort = cohort.empty? ? :november : cohort.downcase
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
+    puts "Please enter another name"
     name = gets.chomp
   end
   students
@@ -17,8 +29,22 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40, "-")
+  # students.each_with_index do |student, index|
+  #     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40, "-")
+  # end
+  students_by_cohort = {}
+  @months.each do |month|
+    students_by_cohort[month.downcase.to_sym] = []
+  end
+
+  students.each do |student|
+    students_by_cohort[student[:cohort]].push(student[:name])
+  end
+  students_by_cohort.each do |month, cohort|
+    puts "#{month.to_s.capitalize} cohort" if !cohort.empty?
+    cohort.each_with_index do |name, index|
+      puts "#{index + 1}. #{name}"
+    end
   end
 end
   
@@ -46,23 +72,21 @@ def filter_by_length(students)
   print_everything(filtered)
 end
 
-
-
 students = input_students
 if students.empty?
-  puts "No students have been entered."
+  puts "No students have been entered. Please run the program again and input some names."
 else
-  puts "Filter by first letter of name or length of name? Type the relevant number: "
-  puts "1. By letter"
-  puts "2. By length (11 character max)"
-  puts "3. No filter"
-  input = gets.chomp.downcase
+  puts "Please select one of the following numbers to pick the relevant option:"
+  puts "1. Print all names unsorted"
+  puts "2. Print only those names beginning with a specific letter"
+  puts "3. Print only those whose names are less than 12 characters long"
+  input = gets.chomp
   if input == "1"
-    filter_by_letter(students)
-  elsif input = "2"
-    filter_by_length(students)
-  else
     print_everything(students)
+  elsif input == "2"
+    filter_by_letter(students)
+  elsif input = "3"
+    filter_by_length(students)
   end
 end
 
